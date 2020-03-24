6d113a2c3d682fd2c4e9763504081a227590572f
Julian <cusco2891@gmail.com>
2020-03-24 19:18:40 -0400

    cleaning

--- server.js
+++ server.js
@@ -1,66 +1,14 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
-// require('dotenv').config();
 
-// const nodemailer = require("nodemailer");
+const PORT = process.env.PORT || 3001;
+// const routes = require("./routes");
+// require('dotenv').config();
+// const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// app.post('/api/form', (req,res) => {
-//   // console.log(req.body)
-//   nodemailer.createTestAccount((err, account) => {
-//     const htmlEmail = `
-//     <h3>Contact Details</h3>
-//     <ul>
-//       <li>Name: ${req.body.name}</li>
-//       <li>Email: ${req.body.email}</li>
-//     </ul>
-//     <h3>Message</h3>
-//     <p>${req.body.message}</p>
-//     `
-
-//     let transporter = nodemailer.createTransport({
-//       host: 'smtp.ethereal.email',
-//       port: 587,
-//       auth: {
-//         user: 'danyka.mosciski@ethereal.email',
-//         pass: 'gTPQ7pzJdnGqf8bjmY'
-//       }
-//     })
-
-//     let mailOptions = {
-//       from:'info@wjbest.com',
-//       to:'danyka.mosciski@ethereal.email',
-//       replyTo:'info@wjbest.com',
-//       subject:'New Message',
-//       text: req.body.message,
-//       html: htmlEmail
-//     }
-
-//     transporter.sendMail(mailOptions, (err,info) => {
-//       if (err) {
-//         return console.log(err)
-//       }
-
-//       // console.log('Message sent: %s', info.message);
-//       // console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
-
-//     })
-//   })
-// });
 
 // app.use(routes);
 
